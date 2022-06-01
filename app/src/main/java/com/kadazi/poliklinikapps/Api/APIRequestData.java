ccf4efdb8ff54be67864af0a01a335e12c5cfb2e
nurulkafi <nurulkafi18@gmail.com>
2022-06-01 07:38:08 +0700

    login

--- app/src/main/java/com/kadazi/poliklinikapps/Api/APIRequestData.java
+++ app/src/main/java/com/kadazi/poliklinikapps/Api/APIRequestData.java
@@ -1,11 +1,22 @@
 package com.kadazi.poliklinikapps.Api;
 
+import com.kadazi.poliklinikapps.Model.ResponseModel;
 import com.kadazi.poliklinikapps.Model.ResponseModelResep;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface APIRequestData {
     @GET("resep")
     Call<ResponseModelResep> listResep();
+
+    @FormUrlEncoded
+    @POST("auth")
+    Call<ResponseModel> login(
+            @Field("email") String email,
+            @Field("password") String password
+    );
 }
---
+++ app/src/main/java/com/kadazi/poliklinikapps/Model/ResponseModel.java
@@ -0,0 +1,22 @@
+package com.kadazi.poliklinikapps.Model;
+
+public class ResponseModel {
+    private boolean success;
+    private String message;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
