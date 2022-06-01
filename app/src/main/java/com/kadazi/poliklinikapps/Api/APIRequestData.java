dce9d1499d812b41c2aeb3f7c55ede32116b74ff
nurulkafi <nurulkafi18@gmail.com>
2022-06-01 07:45:47 +0700

    login

--- app/src/main/java/com/kadazi/poliklinikapps/Api/APIRequestData.java
+++ app/src/main/java/com/kadazi/poliklinikapps/Api/APIRequestData.java
@@ -5,21 +5,19 @@ import com.kadazi.poliklinikapps.Model.ResponseModelResep;
 import com.kadazi.poliklinikapps.Model.ResponseModelRiwayat;
 
 import retrofit2.Call;
-<<<<<<< HEAD
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
-=======
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
->>>>>>> 0ee63dc5c06efa1bfa246c2f3d43bf40a76a96bd
+
 
 public interface APIRequestData {
     @GET("resep")
     Call<ResponseModelResep> listResep();
-<<<<<<< HEAD
+
 
     @FormUrlEncoded
     @POST("auth")
@@ -27,8 +25,8 @@ public interface APIRequestData {
             @Field("email") String email,
             @Field("password") String password
     );
-=======
+
     @GET("riwayat_pemeriksaan/{id}")
     Call<ResponseModelRiwayat> listRiwayat(@Path("id") int id);
->>>>>>> 0ee63dc5c06efa1bfa246c2f3d43bf40a76a96bd
+
 }
