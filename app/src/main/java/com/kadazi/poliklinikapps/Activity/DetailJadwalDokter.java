7a15ee84d4122c108c3ba42034261452d26ce7c8
nurulkafi <nurulkafi18@gmail.com>
2022-06-19 12:09:12 +0700

    Merapihkan UI/UX pembayaran , jadwal dokter

--- app/src/main/java/com/kadazi/poliklinikapps/Activity/DetailJadwalDokter.java
+++ app/src/main/java/com/kadazi/poliklinikapps/Activity/DetailJadwalDokter.java
@@ -1,14 +1,21 @@
 package com.kadazi.poliklinikapps.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.kadazi.poliklinikapps.Adapter.AdapterDataJadwal;
 import com.kadazi.poliklinikapps.Adapter.AdapterDataPoli;
 import com.kadazi.poliklinikapps.Api.APIRequestData;
@@ -42,6 +49,50 @@ public class DetailJadwalDokter extends AppCompatActivity {
         String kode_poli = getIntent().getStringExtra("kode_poli");
         tv_poli = findViewById(R.id.textView4);
         tampilData(kode_poli);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId())
+                {
+                    case R.id.page_1:
+                        startActivity(new Intent(getApplicationContext(),MenuActivity.class));
+                        overridePendingTransition(0,0);
+                        return false;
+                    case R.id.page_2:
+                        startActivity(new Intent(getApplicationContext(),AntrianActivity.class));
+                        overridePendingTransition(0,0);
+                        return false;
+                    case R.id.page_3:
+                        startActivity(new Intent(getApplicationContext(),RiwayatPemeriksaanActivity.class));
+                        overridePendingTransition(0,0);
+                        return false;
+                    case R.id.page_4:
+                        startActivity(new Intent(getApplicationContext(),PengaturanActivity.class));
+                        overridePendingTransition(0,0);
+                        return false;
+                }
+                return false;
+            }
+        });
+        ImageButton btn = findViewById(R.id.btn_kembali);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        SwipeRefreshLayout srlData = findViewById(R.id.srlData);
+        srlData.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                srlData.setRefreshing(true);
+                tampilData(kode_poli);
+                srlData.setRefreshing(false);
+            }
+        });
     }
     public void tampilData(String kode_poli){
         APIRequestData arData = RetroServer.konekRetrofit().create(APIRequestData.class);
--- app/src/main/java/com/kadazi/poliklinikapps/Activity/JadwalPraktekActivity.java
+++ app/src/main/java/com/kadazi/poliklinikapps/Activity/JadwalPraktekActivity.java
@@ -1,13 +1,20 @@
 package com.kadazi.poliklinikapps.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.kadazi.poliklinikapps.Adapter.AdapterDataPoli;
 import com.kadazi.poliklinikapps.Adapter.AdapterDataResep;
 import com.kadazi.poliklinikapps.Api.APIRequestData;
@@ -36,6 +43,50 @@ public class JadwalPraktekActivity extends AppCompatActivity {
         lmData = new GridLayoutManager(this,3);
         rvData.setLayoutManager(lmData);
         tampilData();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId())
+                {
+                    case R.id.page_1:
+                        startActivity(new Intent(getApplicationContext(),MenuActivity.class));
+                        overridePendingTransition(0,0);
+                        return false;
+                    case R.id.page_2:
+                        startActivity(new Intent(getApplicationContext(),AntrianActivity.class));
+                        overridePendingTransition(0,0);
+                        return false;
+                    case R.id.page_3:
+                        startActivity(new Intent(getApplicationContext(),RiwayatPemeriksaanActivity.class));
+                        overridePendingTransition(0,0);
+                        return false;
+                    case R.id.page_4:
+                        startActivity(new Intent(getApplicationContext(),PengaturanActivity.class));
+                        overridePendingTransition(0,0);
+                        return false;
+                }
+                return false;
+            }
+        });
+        ImageButton btn = findViewById(R.id.btn_kembali);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        SwipeRefreshLayout srlData = findViewById(R.id.srlData);
+        srlData.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                srlData.setRefreshing(true);
+                tampilData();
+                srlData.setRefreshing(false);
+            }
+        });
     }
     public void tampilData(){
         APIRequestData arData = RetroServer.konekRetrofit().create(APIRequestData.class);
